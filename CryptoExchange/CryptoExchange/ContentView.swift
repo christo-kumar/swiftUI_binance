272ada9295eefdbe594e8bb14a23ed3037e34722
christo <christoslimit@gmail.com>
2022-07-05 08:48:53 +0530

    First checkin

---
+++ CryptoExchange/CryptoExchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoExchange
+//
+//  Created by Christo Kumar on 05/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoExchange/CryptoExchange/CryptoExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoExchangeApp.swift
+//  CryptoExchange
+//
+//  Created by Christo Kumar on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
